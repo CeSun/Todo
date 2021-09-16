720e86e58dfb321cd8ae8193c12bfb4c6d320cb4
sunce <ce.sun@outlook.com>
2021-09-16 22:41:27 +0800

    初始化项目

---
+++ Todo/Views/MainWindow.axaml.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Todo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            FontFamily = "Microsoft YaHei,Simsun,苹方-简,宋体-简";
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
